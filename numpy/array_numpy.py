db3c401572e0ac1300f8de29a89896eb339e4b64
Denis Lazo <denis.lazo@ucr.ac.cr>
2023-10-02 22:01:57 -0600

    datos de clase array con numpy

---
+++ numpy/array_numpy.py
@@ -0,0 +1,20 @@
+#Normalmente se suele importar como np
+import numpy as np
+
+#Podemos crear un arreglo a partir de 
+array = np.array([1, 2, 3, 4, 5])
+
+# Y lo mostramos
+##print(array) #Se mostraría como una lista, pero los valores no estarían separados por coma
+
+
+#TIPO DEL ARRAY
+type(array)
+
+#Dimenciones del Array
+array.ndim #Muestra filas y columnas. Ej: (2,5)
+
+
+#TIPOS DE DATOS
+array.dtype # Ej. dtype('int64')
+
